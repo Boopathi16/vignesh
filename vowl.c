d4e0ddc86ee731459d390512cf03012521245299
Boopathi16 <rajsurya0070@gmail.com>
2017-04-22 11:06:41 +0530

    Rename vowel.c to vowl.c

---
+++ vowl.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int c;
+if(c=a//c=e//c=i//c=o//c=u)
+{
+printf("%c",the chracter is vowel")
+}else
+{
+printf("%c", the character is consonent");
+}
+return 0;
+}
